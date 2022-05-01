8977e8033af21a76592f7ed16dacc5bf9aa0c513
Matthew Flegg <matthewflegg@outlook.com>
2022-05-01 18:59:51 +0100

    🌟 bootloader now reads from disk

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,2 +1,1 @@
-echo "Running Jinx OS in QEMU..."
-qemu-system-i386 -fda build/main_floppy.img
+bochs -f config/bochsrc.txt
--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -99,23 +99,179 @@ main:
     mov ss, ax
     mov sp, 0x7c00
 
+    ; [TEST] read something from floppy disk
+    ; bios should set dl to drive number
+    mov [ebr_drive_number], dl
+
+    mov ax, 1         ; lba=1, second sector from disk
+    mov cl, 1         ; 1 sector to read
+    mov bx, 0x7e00    ; data should be after the bootloader
+    call disk_read
+
     ; print message
-    mov si, msg
+    mov si, msg_success
     call puts
 
-
-    hlt    ; halt the processor
+    cli               ; disable interrupts
+    hlt               ; halt the processor
 ;
 ; end
 ;
 
 
-msg: db 'Me When!', endl, 0
+;
+; error handlers
+;
 
+;
+; shows error message on disk read failure
+;
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+;
+; end
+;
+
+
+;
+; waits for key press and reboots
+;
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h         ; wait for key press
+    jmp 0ffffh:0    ; jump to beginning of BIOS, should reboot
 
 .halt:
-    jmp .halt
+    cli             ; disable interrupts
+    hlt
+;
+; end
+;
 
+
+;
+; disk routines
+;
+
+;
+; converts an lba address to a chs address
+;
+; params:
+;   - ax : lba address
+;
+; returns:
+;   - cx (bits 0-5) : sector number
+;   - cx (buts 6-15) : cyclinder
+;   - dh : head
+;
+lba_to_chs:
+    ; push ax, dx to the stack
+    push ax
+    push dx
+
+    xor dx, dx                          ; dx = 0
+    div word [bdb_sectors_per_track]    ; ax = lba / sectors per track
+                                        ; dx = lba % sectors per track
+    inc dx                              ; dx = (lba / sectors per track) + 1 = sector
+    mov cx, dx                          ; cx = sector
+    
+    xor dx, dx                          ; dx = 0
+    div word [bdb_heads]                ; ax = (lba / sectors per track) / heads = cylinder
+                                        ; ax = (lba / sectors per track) % heads = head
+    mov dh, dl                          ; dl = head
+    mov ch, al                          ; ch = cylinder (lower 8 bits)
+    shl ah, 6
+    or cl, ah                           ; upper 2 bits of cylinder in cl         
+
+    pop ax
+    mov dl, al                          ; restore dl
+    pop ax
+    ret
+;
+; end
+;
+
+
+;
+; reads sectors from a disk
+;
+; parameters:
+;   - ax : lba address
+;   - cl : number of sectors to read (up to 128)
+;   - dl : drive number
+;   - es/bx : memory address to store read data
+;
+disk_read:
+    ; save registers we will modify
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx            ; temporarily save cl
+    call lba_to_chs    ; compute chs
+    pop ax             ; al = num sectors to read
+
+    mov ah, 02h
+    mov di, 3          ; retry count
+
+.retry:
+    pusha              ; save all registers, we don't know what bios modifies
+    stc                ; set carry flag
+
+    int 13h            ; carry flag cleared - success
+    jnc .done          ; jump if no carry set
+    
+    ; read failed
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    ; after all attempts are exhausted
+    jmp floppy_error
+
+.done:
+    popa
+
+    pop di            ; restore registers modified
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+;
+; end
+;
+
+
+;
+; resets disk controller
+;
+; params:
+;   - dl: drive number
+;
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
+;
+; end
+;
+
+
+msg_success:     db 'Successfully booted.', endl, 0
+msg_read_failed: db 'Failed to read from disk.', endl, 0
 
 times 510-($-$$) db 0    ; pad bytes for the length of the program to 0
 dw 0aa55h                ; befine 2-byte constant
