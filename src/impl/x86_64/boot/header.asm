d364e26e4b3b6a1fdfc8f611e5a6a09a8ad2d12c
Matthew Flegg <matthewflegg@outlook.com>
2022-05-05 21:40:00 +0100

    🌟 first successful build

--- src/impl/x86_64/boot/header.asm
+++ src/impl/x86_64/boot/header.asm
@@ -8,7 +8,7 @@ HEADER_START:
     DD 0xE85250D6                                                     ; Magic number
     DD 0                                                              ; Protected mode i386
     DD HEADER_END - HEADER_START                                      ; Header length
-    DD 0x100000000 - (0xE85250D6 + 0 + HEADER_END - HEADER_START))    ; Checksum
+    DD 0x100000000 - (0xE85250D6 + 0 + (HEADER_END - HEADER_START))    ; Checksum
     
     ; End tag
     DW 0
